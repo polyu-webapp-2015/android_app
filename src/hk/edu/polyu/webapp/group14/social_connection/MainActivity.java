c73a784d351a6380ecdce3796dd36cac667e7f37
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-12-20 16:04:02 +0800

    updated webview to catch all redirect

--- src/hk/edu/polyu/webapp/group14/social_connection/MainActivity.java
+++ src/hk/edu/polyu/webapp/group14/social_connection/MainActivity.java
@@ -3,12 +3,14 @@ package hk.edu.polyu.webapp.group14.social_connection;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
     public static final String host = "58.96.176.223";
     public static final int port = 9080;
     public static final String page = "nav_client.html";
     public static final String server_url = "http://" + host + ":" + port + "/" + page;
+//    public static final String server_url = "http://yahoo.hk";
 
     /**
      * Called when the activity is first created.
@@ -22,6 +24,13 @@ public class MainActivity extends Activity {
 
     private void initWebView() {
         WebView webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                webView.loadUrl(url);
+                return false;
+            }
+        });
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         webView.setNetworkAvailable(true);
