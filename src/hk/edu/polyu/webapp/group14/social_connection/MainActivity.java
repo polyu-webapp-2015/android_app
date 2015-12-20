7753fe5bc6985f6c81a68dcfbfb7aa8373675114
Beeno Tung <aabbcc1241@yahoo.com.hk>
2015-12-20 15:48:33 +0800

    finished basic app

---
+++ src/hk/edu/polyu/webapp/group14/social_connection/MainActivity.java
@@ -0,0 +1,30 @@
+package hk.edu.polyu.webapp.group14.social_connection;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    public static final String host = "58.96.176.223";
+    public static final int port = 9080;
+    public static final String page = "nav_client.html";
+    public static final String server_url = "http://" + host + ":" + port + "/" + page;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        initWebView();
+    }
+
+    private void initWebView() {
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.setNetworkAvailable(true);
+        webView.loadUrl(server_url);
+    }
+}
